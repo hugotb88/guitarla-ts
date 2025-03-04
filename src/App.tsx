9bab28c97b56d4c26f670cdb12032686ddb6b27c
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2025-03-04 11:52:37 -0600

    Removed not used prop

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,6 @@ function App() {
             <Guitar
               key={guitar.id}
               guitar={guitar}
-              cart={cart}
               addToCart={addToCart}
             />
           ))}
