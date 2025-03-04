e975b047f3942127b8930252e7d52045fb35ed16
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2025-03-04 12:11:57 -0600

    Right way to create types, Folder and file to define them

--- src/components/Guitar.tsx
+++ src/components/Guitar.tsx
@@ -1,16 +1,25 @@
+import type { Guitar } from '../types'
+
+// Assign Types to props
+// There are the Guitar Props, thats why we keep them here and not in the index.ts from types folder.
+type GuitarProps = {
+    guitar: Guitar, 
+    addToCart: (item: Guitar) => void
+}
+
 function Guitar({guitar, addToCart} : GuitarProps ) {
 
-    const {id, name, image, description, price} = guitar
+    const {name, image, description, price} = guitar
 
     return (
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
-                <img className="img-fluid" src={`/img/${guitar.image}.jpg`} alt="imagen guitarra" />
+                <img className="img-fluid" src={`/img/${image}.jpg`} alt="imagen guitarra" />
             </div>
             <div className="col-8">
-                <h3 className="text-black fs-4 fw-bold text-uppercase">{guitar.name}</h3>
+                <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
                 <p>{description}</p>
-                <p className="fw-black text-primary fs-3">${guitar.price}</p>
+                <p className="fw-black text-primary fs-3">${price}</p>
                 <button
                     type="button"
                     className="btn btn-dark w-100"
---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+// Folder and file to define your Types (This is the right way to do it)
+
+// Type Guitar  definition
+export type Guitar = {
+    id : number
+    name : string
+    image : string
+    description : string 
+    price : number
+}
