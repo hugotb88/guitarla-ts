c11dbbd87784cba8780a6aaf62517f9ad4869c71
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2025-03-04 13:55:15 -0600

    Utility Types, important

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect, useMemo } from "react"
 import { db } from '../data/db'
-import type { Guitar } from "../types"
+import type { CartItem } from "../types"
 
 // Custom Hook
 // .js file because jsx contains also templates or visuals, a hook should not.
@@ -10,7 +10,7 @@ const useCart = () => {
     const MIN_ITEMS = 1
 
     // Initial Cart state in case it has elements already
-    const initialCart = () : Guitar[] => {
+    const initialCart = () : CartItem[] => {
         const localStorageCart = localStorage.getItem('cart')
         return localStorageCart ? JSON.parse(localStorageCart) : []
     }
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,15 +2,29 @@
 
 // Type Guitar  definition
 export type Guitar = {
-    id : number
-    name : string
-    image : string
-    description : string 
-    price : number
+    id: number
+    name: string
+    image: string
+    description: string
+    price: number
 }
 
 // Type CartItem  definition
 // Inherits all the attributes from Guitar -> Using  'Guitar &'
-export type CartItem = Guitar & {  
-    quantity : number
+export type CartItem = Guitar & {
+    quantity: number
+}
+
+// Utility Types
+// Inherits only some attributes from Guitar
+// You can add new Attributes also with '&'
+export type CartItemUtilityPick = Pick<Guitar, 'id' | 'name' | 'price'> & {
+    quantity: number
+}
+
+// Utility Types
+// Example 2 with 'Omit"
+// It will omit certain attributes
+export type CartItemUtilityOmit = Omit<Guitar, 'description' | 'price'> & {
+    quantity: number
 }
